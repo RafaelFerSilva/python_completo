b3582225f2d076b9cbc2dc8cd0b5f7c3dca5038b
Rafael Fernandes da Silva <rafael.silva@orbia.ag>
2024-06-11 10:59:50 -0300

    Type String

---
+++ BASICO/aula3.py
@@ -0,0 +1,17 @@
+# Python é uma linguagem com tipagem dinâmica / forte
+
+# Aspas simples
+print('Rafael Silva')
+
+# Aspas duplas
+print("Rafael Silva")
+
+# Escape
+print("Rafael \"Silva\"")
+
+# r
+print(r"Rafael \"Silva\"")
+
+print('Rafael "Silva"')
+
+print("Rafael 'Silva'")
