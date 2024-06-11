1a38838b806bff73b0808f49f995e41102cc0616
Rafael Fernandes da Silva <rafael.silva@orbia.ag>
2024-06-11 15:51:30 -0300

    variables

---
+++ BASICO/aula7.py
@@ -0,0 +1,12 @@
+
+nome_completo = 'Rafael Fernandes'
+print(nome_completo)
+
+soma = 2 + 2
+print(soma)
+
+nome = 'Rafael'
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome: ', nome, 'Idade: ', idade)
+print('É maior? ', maior_de_idade)
