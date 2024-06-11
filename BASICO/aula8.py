2a5af286aa2aa96f262abab44f4820ae03984cca
Rafael Fernandes da Silva <rafael.silva@orbia.ag>
2024-06-11 16:08:06 -0300

    Exercises

---
+++ BASICO/aula8.py
@@ -0,0 +1,14 @@
+nome = 'Rafael'
+sobrenome = 'Silva'
+idade = 35
+ano_nascimento = 1989
+ano_atual = 2024
+maior_de_idade = (ano_atual - ano_nascimento) >= 18
+altura = 1.75
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade: ', maior_de_idade)
+print('Altura em metros: ', altura)
