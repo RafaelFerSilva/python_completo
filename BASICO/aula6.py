2b03b2a37c1cdf7a89ff36b6ffaf3e6cacc0a2cb
Rafael Fernandes da Silva <rafael.silva@orbia.ag>
2024-06-11 15:21:09 -0300

    Type convertion

---
+++ BASICO/aula6.py
@@ -0,0 +1,22 @@
+# Conversão de tipos
+
+print(1 + 1)
+
+print('a' + 'b')
+
+# Erro -> Tipos diferentes
+# print('1' + 1)
+
+# Tipos imutáveis
+#  str, int, float, bool
+
+print('1', type('1'))
+
+print(int('1'), type(int('1')))
+
+print(int('1') + 1)
+
+print(bool(' '))
+
+
+print(str(11) + 'b')
